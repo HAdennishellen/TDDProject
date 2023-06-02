b2e27ea333aab181b08788e115e17621019fe53c
Dennis <dennis.hellen@ha.ax>
2023-06-02 10:22:07 +0300

    final commit files in the right place

--- HA-TDD-Chess-main/scripts/ApplicationStop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /webapps/chess
-echo "This version has an if!"
-if [ -f "chess.pid" ]; then
-  kill -15 $(cat chess.pid)
-  rm chess.pid
-fi
--- HA-TDD-Chess-main/src/main/java/ax/ha/tdd/chess/engine/Movement.java
+++
@@ -1,9 +0,0 @@
-package ax.ha.tdd.chess.engine;
-
-public interface Movement {
-
-    boolean canMove(Chessboard board, Square destination);
-
-
-
-}
